24e17de7e1c5efb0cc13c960acaa178b9a9391f6
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2024-11-11 22:48:30 +0700

    feat : Add i10n and init bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:vent_expense_pro/screens/bottom_nav.dart';
+
+const appTitle = "VentExpensePro";
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +14,10 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: BottomNav(),
+      title: appTitle,
     );
   }
 }
---
+++ lib/screens/bottom_nav.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class BottomNav extends StatelessWidget {
+  const BottomNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
+
+    final List<Map<String, dynamic>> tabs = [
+      {'icon': Icons.list, 'label': l10n?.transactions ?? ""},
+      {'icon': Icons.account_balance_wallet, 'label': l10n?.wallets ?? ""},
+      {'icon': Icons.bar_chart, 'label': l10n?.reports ?? ""},
+      {'icon': Icons.settings, 'label': l10n?.settings ?? ""},
+    ];
+
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        body: TabBarView(
+          children: tabs
+              .map((tab) => Center(child: Text(tab['label'] as String)))
+              .toList(),
+        ),
+        bottomNavigationBar: TabBar(
+          tabs: tabs
+              .map((tab) => Tab(icon: Icon(tab['icon'] as IconData), text: tab['label'] as String))
+              .toList(),
+          labelColor: Colors.blue,
+          unselectedLabelColor: Colors.grey,
+          indicatorColor: Colors.transparent,
+        ),
+      ),
+    );
+  }
+}
