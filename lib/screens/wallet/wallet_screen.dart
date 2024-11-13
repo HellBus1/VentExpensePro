c51ac2b0040305b14ba07c90e70b857979e49da7
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2024-11-13 19:44:36 +0700

    feat : Use provider in the screen

--- lib/screens/wallet/wallet_screen.dart
+++ lib/screens/wallet/wallet_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 import 'package:vent_expense_pro/commons/constants/constants.dart' as Constants;
+import 'package:vent_expense_pro/dependency_injection.dart';
+import 'package:vent_expense_pro/screens/wallet/provider/wallet_provider.dart';
 import 'package:vent_expense_pro/screens/wallet/widgets/add_wallet_btn.dart';
 import 'package:vent_expense_pro/screens/wallet/widgets/wallet_btn.dart';
 
@@ -11,53 +14,80 @@ class Wallet {
   Wallet({required this.name, required this.balance});
 }
 
-class WalletScreen extends StatelessWidget {
-  final List<Wallet> wallets = [
-    Wallet(name: 'Cash', balance: '989000'),
-    Wallet(name: 'Bank Account', balance: '1500000'),
-    Wallet(name: 'Savings', balance: '2000000'),
-  ];
-  final additionalLastAddWalletButtonCount = 1;
+class WalletScreen extends StatefulWidget {
+  const WalletScreen({super.key});
 
-  WalletScreen({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _WalletScreenState();
+  }
+}
+
+class _WalletScreenState extends State<WalletScreen> {
+  // final List<Wallet> wallets = [
+  //   Wallet(name: 'Cash', balance: '989000'),
+  //   Wallet(name: 'Bank Account', balance: '1500000'),
+  //   Wallet(name: 'Savings', balance: '2000000'),
+  // ];
+  final additionalLastAddWalletButtonCount = 1;
+  late final WalletProvider _walletProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    _walletProvider = locator<WalletProvider>();
+    _walletProvider.loadWallets();
+  }
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height; 
+    double height = MediaQuery.of(context).size.height;
     var localText = AppLocalizations.of(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(localText?.wallets ?? Constants.EMPTY_STRING),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisExtent: height / 7,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
+    return ChangeNotifierProvider.value(
+      value: _walletProvider,
+      child: Consumer<WalletProvider>(builder: (context, provider, child) {
+        if (provider.isLoading) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        final wallets = provider.wallets;
+        
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(localText?.wallets ?? Constants.EMPTY_STRING),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisExtent: height / 7,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemCount:
+                        wallets.length + additionalLastAddWalletButtonCount,
+                    itemBuilder: (context, index) {
+                      if (index == wallets.length) {
+                        return AddWalletBtn();
+                      }
+                      return WalletBtn(
+                        name: wallets[index].name,
+                        balance: wallets[index].balance.toString(),
+                      );
+                    },
+                  ),
                 ),
-                itemCount: wallets.length + additionalLastAddWalletButtonCount,
-                itemBuilder: (context, index) {
-                  if (index == wallets.length) {
-                    return AddWalletBtn();
-                  }
-                  return WalletBtn(
-                    name: wallets[index].name,
-                    balance: wallets[index].balance,
-                  );
-                },
-              ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      }),
     );
   }
 }
